e4f48243ef8cc00bfe1efabeaf132ba3ed43b5a3
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2023-11-19 18:26:40 -0300

    feat: partial dashboard created

---
+++ app/ui/dashboard/card/card.jsx
@@ -0,0 +1,9 @@
+import styles from './card.modules.css'
+
+const Card = () => {
+  return (
+    <div className={styles.container}>Card</div>
+  )
+}
+
+export default Card
---
+++ app/ui/dashboard/transactins/transactions.jsx
@@ -0,0 +1,9 @@
+import styles from './transactions.modules.css'
+
+const Transactions = () => {
+  return (
+    <div className={styles.container}>Transactions</div>
+  )
+}
+
+export default Transactions
