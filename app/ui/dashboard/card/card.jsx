de566a5764c3c71d4fbf3cc49b358623f8cfe8c0
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2023-12-03 13:27:36 -0300

    feat: create card and transactions

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,8 +1,18 @@
-import styles from './card.modules.css'
+import { MdSupervisedUserCircle } from 'react-icons/md'
+import styles from './card.module.css'
 
 const Card = () => {
   return (
-    <div className={styles.container}>Card</div>
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>Total Users</span>
+        <span className={styles.number}>10.273</span>
+        <span className={styles.detail}>
+          <span className={styles.positive}>12</span> more than previous work
+        </span>
+      </div>
+    </div>
   )
 }
 
--- app/ui/dashboard/transactins/transactions.jsx
+++ app/ui/dashboard/transactins/transactions.jsx
@@ -1,9 +1,51 @@
-import styles from './transactions.modules.css'
+import Image from "next/image";
+import styles from "./transactions.module.css";
+
+const Row = ({ name, status, date, amount }) => {
+  return (
+    <tr>
+      <td>
+        <div className={styles.user}>
+          <Image
+            src="/noavatar.png"
+            alt="avatar"
+            width={40}
+            height={40}
+            className={styles.userImage}
+          />
+          {name}
+        </div>
+      </td>
+      <td>
+        <span className={`${styles.status} ${styles[status]}`}>{status}</span>
+      </td>
+      <td>{date}</td>
+      <td>{amount}</td>
+    </tr>
+  );
+};
 
 const Transactions = () => {
   return (
-    <div className={styles.container}>Transactions</div>
-  )
-}
+    <div className={styles.container}>
+      <h2 className={styles.title}>Latest Transactions</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </tr>
+        </thead>
+        <tbody>
+          <Row name="John Due" status="pending" date="14.02.2024" amount="$3.200" />
+          <Row name="John Due" status="cancelled" date="14.02.2024" amount="$3.200" />
+          <Row name="John Due" status="done" date="14.02.2024" amount="$3.200" />
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default Transactions
+export default Transactions;
