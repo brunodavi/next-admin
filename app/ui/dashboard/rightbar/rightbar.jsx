d47da2edc573ab093694f71c3fb3cb648fea6190
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2023-12-03 16:07:49 -0300

    feat: create chart and rightbar

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,9 +1,79 @@
-import styles from './rightbar.module.css'
+import Image from "next/image";
+import styles from "./rightbar.module.css";
+import { MdPlayCircleFilled, MdReadMore } from "react-icons/md";
+
+const ItemIconButton = (props = { className: "" }) => {
+  const { className, value, Icon, ...restProps } = props;
+
+  return (
+    <button {...restProps} className={`${styles.button} ${className}`}>
+      <Icon />
+      {value}
+    </button>
+  );
+};
+
+const ItemImage = ({ alt, src }) => {
+  return (
+    <div className={styles.bgContainer}>
+      <Image alt={alt} src={src} fill className={styles.bg} />
+    </div>
+  );
+};
+
+const ItemContent = ({ notification, title, subtitle, children }) => {
+  return (
+    <>
+      <span className={styles.notification}>{notification}</span>
+      <h3 className={styles.title}>{title}</h3>
+      <span className={styles.subtitle}>{subtitle}</span>
+      <p className={styles.description}>{children}</p>
+    </>
+  );
+};
+
+const Item = ({ image, content, button } = { image: null }) => {
+  return (
+    <div className={styles.item}>
+      {image}
+      <div className={styles.text}>
+        {content}
+        {button}
+      </div>
+    </div>
+  );
+};
 
 const Rightbar = () => {
   return (
-    <div className={styles.container}>Rightbar</div>
-  )
-}
+    <div className={styles.container}>
+      <Item
+        image={<ItemImage alt="astronaut" src="/astronaut.png" />}
+        content={
+          <ItemContent
+            notification="🔥 Available Now"
+            title="How to use the new version of the admin dashboard"
+            subtitle="Takes 4 minutes to learn"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </ItemContent>
+        }
+        button={<ItemIconButton Icon={MdPlayCircleFilled} value="Watch" />}
+      />
+      <Item
+        content={
+          <ItemContent
+            notification="🚀 Coming Soon"
+            title="New server actions are available, partial pre-rendering is coming up!"
+            subtitle="Boost your productivity"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </ItemContent>
+        }
+        button={<ItemIconButton Icon={MdReadMore} value="Learn" />}
+      />
+    </div>
+  );
+};
 
-export default Rightbar
+export default Rightbar;
