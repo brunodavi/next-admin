9d7f3cf58762ad51aa01865db185da3fb12c08be
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2024-03-02 15:08:34 -0300

    feat: dashboard for users and products

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,6 +1,65 @@
+import styles from "@/app/ui/dashboard/products/products.module.css"
+
+import Pagination from "@/app/ui/dashboard/pagination/pagination"
+import Search from "@/app/ui/dashboard/search/search"
+import Link from "next/link"
+import Image from "next/image"
+
 const ProductsPage = () => {
   return (
-    <div>ProductsPage</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stack</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                IPhone
+              </div>
+            </td>
+            <td>Smart Phone Iphone 14 128GB</td>
+            <td>$6.000</td>
+            <td>Out 29 2023</td>
+            <td>34</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.botton} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.botton} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
   )
 }
 
