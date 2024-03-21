fc0ee590680b4c6b70bf4da3df96cfa034847ae6
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2024-03-21 16:46:20 -0300

    feat: create view product

---
+++ app/dashboard/products/[id]/page.jsx
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+import styles from '@/app/ui/dashboard/products/singleProduct/singleProduct.module.css'
+
+const SingleProductPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.imgContainer}>
+          <Image src="/noproduct.jpg" fill alt="" />
+        </div>
+        Iphone
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <label>Title</label>
+          <input type="text" name="title" placeholder='Iphone' />
+          <label>Price</label>
+          <input type="number" name="price" placeholder='$6.000' />
+          <label>Stack</label>
+          <input type="number" name="stock" placeholder="23" />
+          <label>Color</label>
+          <input type="text" name="color" placeholder='blue' />
+          <label>Size</label>
+          <textarea type="text" name="size" placeholder="500g" />
+          <label>Cat</label>
+          <select name="cat" id="cat">
+            <option value="kitchen">Kitchen</option>
+            <option value="computers">Computers</option>
+          </select>
+          <label>Description</label>
+          <textarea name="desc" id="desc" rows="10" placeholder='description'></textarea>
+          <button type="submit">Update</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default SingleProductPage
